9ec6221f87a25b5efd2356783473a2ab446ea4d2
sajo13 <sajosunny007@gmail.com>
2025-02-12 14:35:12 +0530

    Computed properties
    - A computed property is a reactive, cached value that is based on one or more reactive data properties.

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' , quantity: 0 },
             ],
-            cart: 0
         }
     },
     methods: {
@@ -23,8 +23,20 @@ const app = Vue.createApp({
                 this.cart -= 1;
             }
         },
-        updateImage(variantImage) {
-            this.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product + (this.onSale ? ' is on sale' : '');
+        },
+        image() {
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity;
+        }
+
     }
 })
