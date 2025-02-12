5b43188ac8aa86cabda05c34d202c86a4bafafab
sajo13 <sajosunny007@gmail.com>
2025-02-12 12:49:06 +0530

    Event handling
    - Add to cart and remove from cart and based on color show the image.

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 
-
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/',
-            inventory: 10,
-            onSale: true
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if(this.cart > 0) {
+                this.cart -= 1;
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 })
