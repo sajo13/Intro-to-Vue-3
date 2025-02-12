2a3753789307bbc7d36149ad9237198d923af95d
sajo13 <sajosunny007@gmail.com>
2025-02-12 14:57:04 +0530

    Components and props
    - Components are building block of Vue app.
    - Props is a custom attribute for passing data into a component.

---
+++ components/ProductDetails.js
@@ -0,0 +1,18 @@
+
+// solution
+app.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+        `
+  <ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>
+  `
+})
+// solution
